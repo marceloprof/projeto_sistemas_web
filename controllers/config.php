f7567dfef13b895d5009858a13a840269552a08b
carlosvmlima <carlos-lima@msn.com>
2017-07-15 10:31:18 -0300

    exclusão de arquivos, novo arquivo e alteração arquivo

--- controllers/config.php
+++ controllers/config.php
@@ -5,6 +5,6 @@
 
     $config['db']['host']   = "localhost";
     $config['db']['user']   = "root";
-    $config['db']['pass']   = "123456";
+    $config['db']['pass']   = "";
     $config['db']['dbname'] = "ceadifmg";
     $config['db']['port'] = "3306";
